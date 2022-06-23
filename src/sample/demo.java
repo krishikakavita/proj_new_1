299a183153eb03b6f75a224e360ec9ee12a0b357
Venkys <Venkys@DESKTOP-NIIH1LL>
2022-06-23 09:49:06 +0530

    [KAVITA] -adding my project

---
+++ src/sample/demo.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class demo {
+
+	public static void main(String[] args) {
+		System.out.println("hiiiii.....");
+		System.out.println("helllo");
+		System.out.println("kavita");
+	}
+	
+	
+	
+	
+}
